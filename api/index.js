ab930b6e252905239d56d2f2d3f3e816da44362a
shivakumaralli06 <shivakumaralli06@gmail.com>
2026-08-06 13:42:18 +0530

    fix: add root package.json & modern vercel rewrites for 405 fix in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-// Load environment variables from server/.env
 dotenv.config({ path: path.join(__dirname, '../server/.env') });
 
 const app = require('../server/index');
 
-module.exports = app;
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return app(req, res);
+};
